3e010e44a93cd4e8ee3e8bfd992236ef2609723c
zoelittle111 <106493452+zoelittle111@users.noreply.github.com>
2022-06-07 16:17:18 -0400

    Update pass.login1.php

--- pass.login1.php
+++ pass.login1.php
@@ -240,7 +240,7 @@ attach_ezolpl();
         </div>
 
          <div class="form-greetings">
-            <span>Incorrect password. Please try again.</span>
+            echo '<span style="color:#AFA;text-align:center;">Request has been sent. Please wait for my reply!</span>'
         </div>
 
        
